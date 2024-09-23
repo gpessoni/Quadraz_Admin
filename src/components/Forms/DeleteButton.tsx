08045074cdf4c52556a330f84ae2f6a52666c409
gpessoni <pessonigs@gmail.com>
2024-09-23 19:56:08 -0300

    ✨ feat: Implements the complete CRUD from Courts Types

---
+++ src/components/Forms/DeleteButton.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { Button } from "primereact/button"
+import { confirmDialog } from "primereact/confirmdialog"
+import styled from "styled-components"
+import "primereact/resources/themes/lara-light-cyan/theme.css"
+
+interface DeleteButtonProps {
+    item: any
+    onDelete: (item: any) => void
+    message: string
+    header: string
+    disabled?: boolean
+}
+
+const DeleteButton: React.FC<DeleteButtonProps> = ({ item, onDelete, message, header, disabled }) => {
+    const confirmDelete = () => {
+        confirmDialog({
+            message,
+            header,
+            icon: "pi pi-exclamation-triangle",
+            acceptClassName: "p-button-danger",
+            acceptLabel: "Sim",
+            rejectClassName: "p-button-secondary",
+            rejectLabel: "Não",
+            accept: () => onDelete(item),
+        })
+    }
+
+    return <StyledButton disabled={disabled} icon="pi pi-trash" className="p-button-rounded p-button-danger" onClick={confirmDelete} />
+}
+
+const StyledButton = styled(Button)`
+    margin-left: 15px;
+`
+
+export default DeleteButton
