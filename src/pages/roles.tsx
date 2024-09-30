595cd87a03a9dcb8475b1dd5fff50a2ace1e9259
gpessoni <pessonigs@gmail.com>
2024-09-30 13:00:47 -0300

    ✨ feat: Implements the complete CRUD from Roles's

---
+++ src/pages/roles.tsx
@@ -0,0 +1,321 @@
+"use client"
+
+import DeleteButton from "@/components/Forms/DeleteButton"
+import "primeicons/primeicons.css"
+import { Button } from "primereact/button"
+import { Column } from "primereact/column"
+import { ConfirmDialog } from "primereact/confirmdialog"
+import { DataTable, DataTableFilterMeta } from "primereact/datatable"
+import { Dialog } from "primereact/dialog"
+import { InputText } from "primereact/inputtext"
+import "primereact/resources/themes/lara-light-cyan/theme.css"
+import { Skeleton } from "primereact/skeleton"
+import { Toast } from "primereact/toast"
+import { Toolbar } from "primereact/toolbar"
+import { useEffect, useRef, useState } from "react"
+import Navbar from "@/components/navbar"
+import "primereact/resources/themes/lara-light-cyan/theme.css"
+
+type Equipament = {
+    id: string | null
+    name: string
+    createdAt?: Date
+    updatedAt?: Date
+}
+
+export default function Equipaments() {
+    const [loading, setLoading] = useState<boolean>(true)
+    const [roles, setRoles] = useState<Equipament[]>([])
+    const [roleDialog, setRoleDialog] = useState<boolean>(false)
+    const [role, setEquipament] = useState<Equipament>({
+        id: null,
+        name: "",
+    })
+    const [submitted, setSubmitted] = useState<boolean>(false)
+    const toast = useRef<Toast>(null)
+
+    const [filters, setFilters] = useState<DataTableFilterMeta>({
+        name: { value: null, matchMode: "contains" },
+        createdAt: { value: null, matchMode: "contains" },
+        updatedAt: { value: null, matchMode: "contains" },
+    })
+
+    const onFilterChange = (e: React.ChangeEvent<HTMLInputElement>, field: string) => {
+        const newFilters: any = { ...filters }
+        newFilters[field].value = e.target.value
+        setFilters(newFilters)
+    }
+
+    const getAuthToken = () => {
+        return localStorage.getItem("authToken") || ""
+    }
+
+    useEffect(() => {
+        fetchEquipament()
+        setLoading(false)
+    }, [])
+
+    const fetchEquipament = async () => {
+        try {
+            const response = await fetch("/api/roles/list", {
+                headers: {
+                    Authorization: `Bearer ${getAuthToken()}`,
+                },
+            })
+            const data = await response.json()
+            setRoles(data)
+        } catch (error) {
+            console.error("Erro ao buscar Cargos:", error)
+            toast.current?.show({
+                severity: "error",
+                summary: "Erro",
+                detail: "Erro ao buscar Cargos",
+                life: 3000,
+            })
+        }
+    }
+
+    const leftToolbarTemplate = () => {
+        return (
+            <div className="flex justify-between w-full">
+                <Button label="Novo" icon="pi pi-user-plus" className="p-button-success" onClick={openNew} />
+            </div>
+        )
+    }
+
+    const rightToolbarTemplate = () => {
+        return (
+            <div className="flex justify-between w-full">
+                <InputText
+                    id="globalFilter"
+                    placeholder="Pesquisar"
+                    onInput={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        setFilters({ ...filters, global: { value: e.target.value, matchMode: "contains" } })
+                    }
+                    className="ml-2"
+                />
+            </div>
+        )
+    }
+
+    const openNew = () => {
+        setEquipament({ id: null, name: "" })
+        setSubmitted(false)
+        setRoleDialog(true)
+    }
+
+    const hideDialog = () => {
+        setSubmitted(false)
+        setRoleDialog(false)
+    }
+
+    const saveEquipament = async () => {
+        setSubmitted(true)
+
+        if (role.name.trim()) {
+            let _equipament = [...roles]
+            try {
+                console.log("Payload enviado:", JSON.stringify(role))
+
+                const isUpdating = !!role.id
+
+                // Filtra os campos proibidos antes de enviar
+                const { id, createdAt, updatedAt, ...equipamentData } = role
+
+                const response = await fetch(isUpdating ? `/api/roles/update/${role.id}` : "/api/roles/create", {
+                    method: isUpdating ? "PUT" : "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${getAuthToken()}`,
+                    },
+                    body: JSON.stringify(isUpdating ? equipamentData : { name: role.name }),
+                })
+
+                if (!response.ok) {
+                    const errorData = await response.json()
+                    const errorMessage = errorData.error || "Erro ao salvar Cargo"
+                    throw new Error(errorMessage)
+                }
+
+                const result = await response.json()
+                if (!isUpdating) {
+                    _equipament.push(result)
+                } else {
+                    _equipament[_equipament.findIndex((g) => g.id === role.id)] = result
+                }
+
+                toast.current?.show({
+                    severity: "success",
+                    summary: "Sucesso",
+                    detail: isUpdating ? "Cargo atualizado com sucesso" : "Cargo criado com sucesso",
+                    life: 3000,
+                })
+                setRoles(_equipament)
+                setRoleDialog(false)
+                setEquipament({ id: null, name: "" })
+            } catch (error) {
+                console.error("Erro ao salvar Cargo:", error)
+                const errorMessage = error instanceof Error ? error.message : "Erro desconhecido ao salvar Cargo"
+                toast.current?.show({
+                    severity: "error",
+                    summary: "Erro",
+                    detail: errorMessage,
+                    life: 4000,
+                })
+            }
+        } else {
+            toast.current?.show({
+                severity: "warn",
+                summary: "Atenção",
+                detail: "Preencha todos os campos obrigatórios.",
+                life: 3000,
+            })
+        }
+    }
+
+    const editEquipament = (role: Equipament) => {
+        setEquipament({ ...role })
+        setRoleDialog(true)
+    }
+
+    const deleteEquipament = async (role: Equipament) => {
+        try {
+            const response = await fetch(`/api/roles/delete/${role.id}`, {
+                method: "DELETE",
+                headers: {
+                    Authorization: `Bearer ${getAuthToken()}`,
+                },
+            })
+
+            if (response.ok) {
+                let _equipament = roles.filter((val) => val.id !== role.id)
+                setRoles(_equipament)
+                toast.current?.show({
+                    severity: "success",
+                    summary: "Sucesso",
+                    detail: "Cargo deletado com sucesso",
+                    life: 3000,
+                })
+            } else {
+                throw new Error("Erro ao deletar Cargo")
+            }
+        } catch (error) {
+            console.error("Erro ao deletar Cargo:", error)
+            toast.current?.show({
+                severity: "error",
+                summary: "Erro",
+                detail: "Erro ao deletar Cargo",
+                life: 3000,
+            })
+        }
+    }
+
+    const roleDialogFooter = (
+        <>
+            <Button label="Cancelar" icon="pi pi-times" className="p-button-danger" onClick={hideDialog} />
+            <Button label="Salvar" icon="pi pi-check" className="p-button-success" onClick={saveEquipament} />
+        </>
+    )
+
+    if (loading) {
+        return <Skeleton shape="rectangle" width="100%" height="100vh" />
+    }
+
+    return (
+        <>
+            <Navbar />
+            <Toast ref={toast} />
+            <div className="card">
+                <Toolbar className="p-mb-4 p-toolbar" left={leftToolbarTemplate} right={rightToolbarTemplate}></Toolbar>
+                <DataTable
+                    filters={filters}
+                    onFilter={(e) => setFilters(e.filters as DataTableFilterMeta)}
+                    globalFilterFields={["name", "createdAt", "updatedAt"]}
+                    header="Cargos"
+                    style={{
+                        width: "100%",
+                        overflow: "auto",
+                        border: "1px solid #ccc",
+                    }}
+                    value={roles}
+                    paginator
+                    rows={7}
+                    rowsPerPageOptions={[7, 10, 25, 50]}
+                >
+                    <Column align="center" field="name" header="Tipo" sortable></Column>
+                    <Column
+                        align="center"
+                        field="createdAt"
+                        header="Data de Criação"
+                        body={(rowData) => {
+                            const date = new Date(rowData.createdAt)
+                            return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+                        }}
+                        sortable
+                    ></Column>
+                    <Column
+                        align="center"
+                        field="updatedAt"
+                        header="Data de Atualização"
+                        body={(rowData) => {
+                            const date = new Date(rowData.updatedAt)
+                            return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+                        }}
+                        sortable
+                    ></Column>
+                    <Column
+                        align="center"
+                        body={(rowData: Equipament) => (
+                            <>
+                                <div style={{ display: "flex", flexWrap: "nowrap" }}>
+                                    <Button
+                                        icon="pi pi-pencil"
+                                        className="p-button-rounded p-button-success mr-2"
+                                        onClick={() => editEquipament(rowData)}
+                                    />
+                                    <DeleteButton
+                                        item={rowData}
+                                        onDelete={deleteEquipament}
+                                        message={`Você tem certeza que deseja deletar o equipamento ${rowData.name}?`}
+                                        header="Confirmação"
+                                    />
+                                </div>
+                            </>
+                        )}
+                    />
+                </DataTable>
+
+                <Dialog
+                    visible={roleDialog}
+                    style={{ width: "450px" }}
+                    draggable={false}
+                    header="Detalhes do Cargo"
+                    modal
+                    className="p-fluid"
+                    footer={roleDialogFooter}
+                    onHide={hideDialog}
+                >
+                    <div
+                        className="field"
+                        style={{
+                            marginTop: "10px",
+                        }}
+                    >
+                        <label htmlFor="description">Tipo de Cargo</label>
+                        <InputText
+                            id="description"
+                            value={role.name}
+                            onChange={(e) => setEquipament({ ...role, name: e.target.value })}
+                            style={{
+                                marginTop: "10px",
+                            }}
+                            required
+                            className={submitted && !role.name ? "p-invalid" : ""}
+                        />
+                    </div>
+                </Dialog>
+                <ConfirmDialog />
+            </div>
+        </>
+    )
+}
